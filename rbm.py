d4453f4eb6e841572bbbc834e75c7aeb3d8bc6b9
bargacha <bargachayoub@gmail.com>
2018-03-22 21:34:57 +0100

    Data ok

---
+++ rbm.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from random import shuffle
+import os
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# ----- DATAGEN PART -----
+
+# Array with 10000 (8000 trn and 2000 tst) of 784-dim vectors representing matrices of 28x28
+
+os.chdir( os.path.dirname(os.path.abspath(__file__)) )
+
+t_trn_f = open("binMNIST_data/bindigit_trn.csv", "r")
+reader = csv.reader(t_trn_f)
+
+pics = [ [int(row[i]) for i in range( 0, len(row) ) ] for row in reader]
+
+t_trn_f.close()
+
+t_tst_f = open("binMNIST_data/bindigit_tst.csv", "r")
+reader = csv.reader(t_tst_f)
+
+pics += [ [int(row[i]) for i in range( 0, len(row) ) ] for row in reader]
+
+t_tst_f.close()
+
+
+# Matrix of target classifications
+
+trn_f = open("binMNIST_data/targetdigit_trn.csv", "r")
+reader = csv.reader(trn_f)
+
+cls = [ int(row[0]) for row in reader]
+
+trn_f.close()
+
+trn_f = open("binMNIST_data/targetdigit_tst.csv", "r")
+reader = csv.reader(trn_f)
+
+cls += [ int(row[0]) for row in reader]
+
+trn_f.close()
+
+
+# ----- DATA READY -----
+
+
+
+
+
+
+
+
+
+
