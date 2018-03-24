21b100bc59db51369a1328acafeef0de38b2d049
bargacha <bargachayoub@gmail.com>
2018-03-24 16:59:53 +0100

    Picture mgt added

--- rbm.py
+++ rbm.py
@@ -15,6 +15,41 @@ from sklearn.metrics.classification import accuracy_score
 
 from dbn import SupervisedDBNClassification
 
+
+# ----- CLASSES -----
+class picture:
+	""" Class to handle automatically the plot management.
+	By using the defined classes you can automatically add vectors the general plot.
+	Create multiple instances to have multiple plot frames
+	"""
+
+	def __init__(self, x_dim, y_dim, total) :
+		fig = plt.figure()
+		x = x_dim
+		y = y_dim
+		rows = int( math.sqrt( total ) )
+		if ( total % rows ) == 0 :
+			columns = rows
+		else :
+			columns = rows + 1
+
+		counter = 1
+
+	def add_vector(self, vector) : 
+		if total > counter and i * j == len( vector ) :
+			# We start by transforming the vector to a matrix
+			matrix = [ [ int( vector[ i + j * x] ) for i in range( 0, x ) ] for j in range( 0, y ) ]
+			counter += 1
+			fig.add_subplot(rows, columns, counter)
+			plt.imshow( matrix )
+		else :
+			print( "WARNING : a vector have been ommited due to overtacked number of graphs or unapropriate vector length." )
+
+	def show_pictures(self, vector) :
+		plt.show()
+		
+
+
 # ----- DATAGEN PART -----
 
 # Array with 10000 (8000 trn and 2000 tst) of 784-dim vectors representing matrices of 28x28
@@ -65,11 +100,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(pics, cls, test_size=0.2, ra
 
 # ----- TRAINING -----
 
-classifier = SupervisedDBNClassification(hidden_layers_structure=[256, 256],
+classifier = SupervisedDBNClassification(hidden_layers_structure=[50],
                                          learning_rate_rbm=0.05,
                                          learning_rate=0.1,
                                          n_epochs_rbm=10,
-                                         n_iter_backprop=100,
+                                         n_iter_backprop=50,
                                          batch_size=32,
                                          activation_function='relu',
                                          dropout_p=0.2)
@@ -86,7 +121,7 @@ classifier = SupervisedDBNClassification.load('model.pkl')
 Y_pred = classifier.predict(X_test)
 print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
 
-
+# ----- PLOT -----
 
 
 
