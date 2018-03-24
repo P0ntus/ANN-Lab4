ac348297b3857a520ced1949d9da3254499fc1a2
bargacha <bargachayoub@gmail.com>
2018-03-24 12:48:53 +0100

    dbn

--- rbm.py
+++ rbm.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-from random import shuffle
 import os
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+
+# Additionnal ANN packages
+
+np.random.seed(1337)  # for reproducibility
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.classification import accuracy_score
+
+from dbn import SupervisedDBNClassification
 
 # ----- DATAGEN PART -----
 
@@ -44,10 +52,42 @@ cls += [ int(row[0]) for row in reader]
 
 trn_f.close()
 
+# Convert to numpy arrays
+pics = np.array( pics )
+cls = np.array( cls )
 
-# Use pics for pictures and cls for the targeted classification
+
+# Use pics for pictures and cls to create data parts
+
+X_train, X_test, Y_train, Y_test = train_test_split(pics, cls, test_size=0.2, random_state=0) # As we have 8000 for trn and 2000 for tst
 
 # ----- DATA READY -----
+
+# ----- TRAINING -----
+
+classifier = SupervisedDBNClassification(hidden_layers_structure=[256, 256],
+                                         learning_rate_rbm=0.05,
+                                         learning_rate=0.1,
+                                         n_epochs_rbm=10,
+                                         n_iter_backprop=100,
+                                         batch_size=32,
+                                         activation_function='relu',
+                                         dropout_p=0.2)
+classifier.fit(X_train, Y_train)
+
+# Save the model
+classifier.save('model.pkl')
+
+# Restore it
+classifier = SupervisedDBNClassification.load('model.pkl')
+
+# ----- TEST -----
+
+Y_pred = classifier.predict(X_test)
+print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
+
+
+
 
 
 
