13b0101713b01c8d04a9707bb469da5e6cb84cd7
P0ntus <pontustobiasson@gmail.com>
2018-03-28 15:37:03 +0200

    more work on auto encoder

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -14,6 +14,20 @@ np.set_printoptions(threshold=np.nan) #Always print the whole matrix
 
 # ----- FUNCTIONS START -----
 
+# Count how many bits differ between pattern x and y
+def count_errors(x, y):
+	count = 0
+	for i in range(0, len(x)):
+		if (x[i] != y[i]):
+			count = count + 1
+	return(count)
+
+def calculate_average_error(x, y):
+	error = 0
+	for i in range(0, len(x)):
+		error += abs(x[i] - y[i])
+	return(error)
+
 # Change targets of form 0-9 to something like 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
 def binary_target(input) :
 	output = [0]*10
@@ -84,18 +98,47 @@ test_target = np.array(test_target)
 
 # Training Parameters
 learning_rate = 0.1
-num_steps = 1000
+num_steps = 500
 #batch_size = 256
 
 display_step = 50
 
 # Network Parameters
-num_hidden_1 = 256 # 1st layer num features
-num_hidden_2 = 128 # 2nd layer num features (the latent dim)
+num_hidden_1 = 10 #256 # 1st layer num features
+num_hidden_2 = 5 #128 # 2nd layer num features (the latent dim)
 num_input = 784 # MNIST data input (img shape: 28*28)
 
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, num_input])
+
+'''
+# ----- SINGLE LAYER START -----
+
+weights = {
+	'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1])),
+	'decoder_h1': tf.Variable(tf.random_normal([num_hidden_1, num_input])),
+}
+
+biases = {
+	'encoder_b1': tf.Variable([float(0)]*num_hidden_1),
+	'decoder_b1': tf.Variable([float(0)]*num_input),
+}
+
+# Building the encoder
+def encoder(x):
+	# Encoder Hidden layer with sigmoid activation #1
+	layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
+	return layer_1
+
+# Building the decoder
+def decoder(x):
+	# Decoder Hidden layer with sigmoid activation #1
+	layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
+	return layer_1
+
+# ----- SINGLE LAYER END -----
+'''
+# ----- MULTI LAYER START -----
 
 weights = {
 	'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1])),
@@ -103,11 +146,12 @@ weights = {
 	'decoder_h1': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1])),
 	'decoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_input])),
 }
+
 biases = {
-	'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1])),
-	'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2])),
-	'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1])),
-	'decoder_b2': tf.Variable(tf.random_normal([num_input])),
+	'encoder_b1': tf.Variable([float(0)]*num_hidden_1),
+	'encoder_b2': tf.Variable([float(0)]*num_hidden_2),
+	'decoder_b1': tf.Variable([float(0)]*num_hidden_1),
+	'decoder_b2': tf.Variable([float(0)]*num_input),
 }
 
 # Building the encoder
@@ -126,6 +170,9 @@ def decoder(x):
 	layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
 	return layer_2
 
+
+# ----- MULTI LAYER END -----
+
 # Construct model
 encoder_op = encoder(X)
 decoder_op = decoder(encoder_op)
@@ -139,28 +186,47 @@ y_true = X
 
 loss = tf.reduce_mean(abs(y_true - y_pred))
 
+L1 = [tf.trainable_variables()[1]]
+opt_L1 = tf.train.RMSPropOptimizer(learning_rate).minimize(loss, var_list = L1)
+print(L1)
 optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
 #optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
-print(tf.trainable_variables()) # Print variables
 
 # Start Training
 # Start a new TF session
 with tf.Session() as sess:
 
+
 	# Run the initializer
 	sess.run(init)
-			
+	
+	for v in tf.trainable_variables(): # Print all tf variables
+		print(v)
+	print(tf.trainable_variables()[1].eval(sess)) # Get session variable	
+	print(tf.trainable_variables()[2].eval(sess))
+	
 	# Training
 	for i in range(1, num_steps+1):
 		# Run optimization op (backprop) and cost op (to get loss value)
-		training_target, l = sess.run([optimizer, loss], feed_dict={X: training_input})
+		training_target, l = sess.run([opt_L1, loss], feed_dict={X: training_input})
 		# Display logs per step
 		if i % display_step == 0 or i == 1:
-			print('Step %i: Minibatch Loss: %f' % (i, l))			
-				
+			print('Step %i: Loss: %f' % (i, l))
+			'''
+			# Apparently aleady calculated using the tf.reduce_mean thing
+			error = 0
+			training_output = sess.run(decoder_op, feed_dict={X: training_input})
+			for j in range(0, len(training_input)):
+				error += calculate_average_error(training_input[j], training_output[j])
+			print("Total error:: ", error, "Average error:", error/len(training_input), "Average error (between 1-0):", error/(len(training_input)*784))
+			'''
+			
+	print(tf.trainable_variables()[1].eval(sess)) # Get session variable	
+	print(tf.trainable_variables()[2].eval(sess))
+			
 	# Testing
 	# Encode and decode images from test set and visualize their reconstruction.
 	rows = 2
@@ -185,8 +251,9 @@ with tf.Session() as sess:
 			canvas_recon[i * 28:(i + 1) * 28, j * 28:(j + 1) * 28] = \
 				test_output[order[i*columns + j]].reshape([28, 28])
 
-	print(tf.trainable_variables()[5].eval(sess)) # Get session variable
-
+	weights = tf.trainable_variables()[1].eval(sess) # Get session variable	
+	
+'''
 print("Original Images")
 plt.figure(figsize=(rows, columns))
 plt.imshow(canvas_orig, origin="upper", cmap="gray")
@@ -196,3 +263,33 @@ print("Reconstructed Images")
 plt.figure(figsize=(rows, columns))
 plt.imshow(canvas_recon, origin="upper", cmap="gray")
 plt.show()
+'''
+
+'''
+# Print weights
+size = num_hidden_1
+# Calculate number of rows and columns needed to display all weight matrices
+columns = math.ceil(math.sqrt(size))
+rows = 1
+count = rows * columns
+while(count < size):
+	rows += 1
+	count = rows * columns
+
+# Add all weight matrices
+weights
+weight_pics = np.empty((28 * rows, 28 * columns))
+for r in range(0, rows):
+	for c in range(0, columns):
+		if(r*columns+c >= size):
+			break
+		print(r*columns+c)
+		weight_pics[r * 28:(r + 1) * 28, c * 28:(c + 1) * 28] = \
+				weights[r*columns + c].reshape([28, 28])
+
+print(rows)
+print("Weight Images")
+plt.figure(figsize=(rows, columns))
+plt.imshow(weight_pics, origin="upper", cmap="gray")
+plt.show()
+'''
